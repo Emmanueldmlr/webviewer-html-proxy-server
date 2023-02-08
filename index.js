0e024002132f5bfb9ea0e07dc5e6395ed046374e
Damilare Bakare <damilarebakare@Damilares-MBP.lan>
2023-02-08 01:05:12 +0100

    changes port

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ const app = express();
 
   app.use(cookieParser());
   app.use(cors());
+  const port = process.env.PORT || 3000;
+
 
   app.get('/', (req, res) => {
     res.send('Hello World!')
   })
 
-app.listen(3000, () => {
-    console.log(`Example app listening on port ${3000}`)
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
   })
